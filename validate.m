fee14a264c089644d0b3cc7d1fa8db382a36b2bc
lmburke <sfzbass@gmail.com>
2017-03-11 22:27:03 -0800

    cleaning up

--- validate.m
+++
@@ -1,7 +0,0 @@
-function [TP,FN,FP,TN] = validate(negIdx,posIdx,pre)
-    % Perform error anlysis
-    TP = sum( pre(posIdx) == 1 );
-    TN = sum( pre(negIdx) == 0 );
-    FN = sum( pre(posIdx) == 0 );
-    FP = sum( pre(negIdx) == 1 );
-end
