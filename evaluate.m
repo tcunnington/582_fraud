a34651df5d0b457f31aed1c83bb95af5f4eb2394
lmburke <sfzbass@gmail.com>
2017-03-16 17:54:16 -0700

    more thorough error tracking; svm

--- evaluate.m
+++ evaluate.m
@@ -1,9 +1,12 @@
 function s = evaluate(model, testData, testClasses)
-
     s = struct;
     s.model = model;
-    s.prediction = predict(model, testData);
-    s.confusion = confusionmat(testClasses,s.prediction);
-    [s.TPR, s.FPR, s.TNR, s.FNR, s.ACC, s.PPV] = analyzePerformance(testClasses,s.prediction);
-
+    [s.labels,s.scores,s.costs] = predict(model, testData);
+    s.confusion = confusionmat(testClasses,s.labels);
+    [s.rocX,s.rocY,s.rocT,s.AUC] = perfcurve(testClasses,s.scores(:,2),'1');
+    [s.prX,s.prY,s.prT,s.AUCPR] = perfcurve(testClasses,s.scores(:,2),'1','XCrit','reca','YCrit','prec');
+%     disp('Bootstrapping ROC')
+%     [s.rocX,s.rocY,s.rocT,s.AUC] = perfcurve(testClasses,s.scores(:,2),'1','NBoot',1,'TVals',0:0.05:1);
+%     disp('Bootstrapping PR')
+%     [s.prX,s.prY,s.prT,s.AUCPR] = perfcurve(testClasses,s.scores(:,2),'1','XCrit','reca','YCrit','prec','NBoot',1,'TVals',0:0.05:1);
 end
--- main.m
+++ main.m
@@ -1,15 +1,19 @@
-clear,close,%clc;
+clear,close all
 tic
 disp('Loading data and initializing')
+%% Parameters
+% Ratio of training to test subsets
+ratio = 0.8;
+% Desired ratio of class 0 to class 1 for ADASYN
+classBalance = 0.1; 
+% Cost of false negative for cost models
+costs = [10 100 10^5 10^10];
+
 %% Initialize variables
 % amount  | amount of transaction in cents
 % time    | time of transaction in seconds
 % class   | boolean fraudulent/legitimate-->1/0
 % data    | features (US in svd USV')
-% n       | total number of transactions
-% n0      | number of legitimate transactions
-% n1      | number of fraudulent transactions
-% fracpos | fraction of fraudulent transactions 
 
 if exist('data.mat', 'file') == 2
     load('data.mat')
@@ -17,44 +21,19 @@ else
     readData
 end
 
-runTree = false;
-
-n = length(time);
-n0 = sum(class==0);
-n1 = sum(class==1);
-fracpos = n1/n;
-
-full = [data amount]; % time is not really important
-% posData = full(class == 1,:);
-% negData = full(class == 0,:);
-
-%% Explore feature space
-% Singular values and POD modes
-S = zeros(size(data,2),1);
-U = zeros(size(data));
-for i=1:size(data,2)
-    S(i)=norm(data(:,i));
-    U(:,i)=data(:,i)/S(i);
-end
-% plot(S)
-% plotFeatureSpace(U(class==1,:), U(class==0,:), [1 2 3]);
-% plotFeatureSpace(posData, negData, [3 10 29]);
-
+data = [data amount]; % time does not seem to improve results
 
 %% Test and train subsets
 % TODO use kfold cross validation instead--it's better and actually easier
 % to implement.........
-ratio = 0.8;
-[trainData, testData, trainClasses, testClasses] = splitBinaryClassData(ratio, full, class);
+[trainData, testData, trainClasses, testClasses] = ...
+    splitBinaryClassData(ratio, data, class);
 
 %% Undersampling / Oversampling / ADASYN
-% TODO apply ADASYN only to the training data....
 disp(['Running ADASYN (',num2str(toc),')'])
 % Scaling is required for ADASYN's internal KNN search
 Ztrain = zscore(trainData);
 Ztest = zscore(testData);
-% ratiopos = n1/n0;
-classBalance = 0.1; % desired ratio of class 0 to class 1
 [synZ, synClassOut] = ADASYN(Ztrain, trainClasses, classBalance);
 synFull = [Ztrain; synZ];
 synClass = [trainClasses; synClassOut];
@@ -62,32 +41,111 @@ synClass = [trainClasses; synClassOut];
 %% Classify
 disp(['Building/ running classification models (',num2str(toc),')'])
 % Binary classification decision tree
-if runTree
-    treefit = fitctree(trainData,trainClasses); %,'OptimizeHyperparameters','auto');
-    tree = evaluate(treefit, testData, testClasses);
-end
+disp(['Binary classification decision tree (',num2str(toc),')']);
+tree = evaluate(fitctree(Ztrain,trainClasses), Ztest, testClasses);
+
+% Support vector machine
+disp(['Support vector machine (',num2str(toc),')']);
+svm = evaluate(fitcsvm(Ztrain, trainClasses), Ztest, testClasses);
 
 % Linear discriminant analysis
-[iTrainSyn, ~] = splitIndices(size(synFull,1),ratio);
-% Baseline / default
+disp(['Linear discriminant analyses (',num2str(toc),')']);
 lda = evaluate(fitcdiscr(Ztrain, trainClasses), Ztest, testClasses);
-
-% LDA with cusotm cost function
-% NOTE: if you plan to vary cost many times use a single model and update
-% model.Cost then evaluate again.
-costs = [10 100 10^5 10^10];
-costModel = fitcdiscr(Ztrain, trainClasses,'ClassNames',[0,1]);
-for i=1:length(costs) % TODO is something wrong here?? all perf. values are exactly the same as vanilla LDA......
-    costModel.Cost = [0 1; costs(i) 0];
-%     R = confusionmat(costModel.Y,resubPredict(costModel))
-    ldaCosts(i) = evaluate(costModel, testData, testClasses);
-end
 
 % LDA with synthetic data -- train with syn data but test with original
 ldaSyn = evaluate(fitcdiscr(synFull, synClass), Ztest, testClasses);
 
+% LDA with custom cost function
+costModel = fitcdiscr(Ztrain, trainClasses,'ClassNames',[0,1]);
+for i=1:length(costs) % TODO is something wrong here?? all perf. values are exactly the same as vanilla LDA......
+    costModel.Cost = [0 1; costs(i) 0];
+    ldaCosts(i) = evaluate(costModel, testData, testClasses);
+end
+
 disp(['Done classifying (',num2str(toc),')'])
-    
+
+
+%% Plot
+% lda
+plotModels = {lda};
+modelNames = {'LDA'};
+AUCs = lda.AUC;
+AUCPRs = lda.AUCPR;
+% ldaSyn
+plotModels = {plotModels{:},ldaSyn};
+modelNames = {modelNames{:},'LDA w/ADASYN'};
+AUCs = [AUCs;ldaSyn.AUC];
+AUCPRs = [AUCPRs;ldaSyn.AUCPR];
+% tree
+plotModels = {plotModels{:},tree};
+modelNames = {modelNames{:},'Tree'};
+AUCs = [AUCs;tree.AUC];
+AUCPRs = [AUCPRs;tree.AUCPR];
+% svm
+plotModels = {plotModels{:},svm};
+modelNames = {modelNames{:},'SVM'};
+AUCs = [AUCs;svm.AUC];
+AUCPRs = [AUCPRs;svm.AUCPR];
+% ldaCosts
+for i=1:length(costs)
+    plotModels = {plotModels{:},ldaCosts(i)};
+    modelNames = {modelNames{:},['LDA w/Costs ',num2str(i)]};
+    AUCs = [AUCs;ldaCosts(i).AUC];
+    AUCPRs = [AUCPRs;ldaCosts(i).AUCPR];
+end
+
+T = table(AUCs,AUCPRs,'RowNames',modelNames)
+
+% ROC Curve
+figure
+hold on
+for i=1:length(plotModels)
+    mdl = plotModels{i};
+%     errorbar(mdl.rocX,mdl.rocY(:,1),mdl.rocY(:,1)-mdl.rocY(:,2),mdl.rocY(:,3)-mdl.rocY(:,1));
+    plot(plotModels{i}.rocX,plotModels{i}.rocY);
+end
+hold off
+legend(modelNames,'Location','Best');
+xlabel('False positive rate') 
+ylabel('True positive rate')
+title('ROC Curves')
+
+% PR Curve
+figure
+hold on
+for i=1:length(plotModels)
+    mdl = plotModels{i};
+%     errorbar(mdl.prX,mdl.prY(:,1),mdl.prY(:,1)-mdl.prY(:,2),mdl.prY(:,3)-mdl.prY(:,1));
+    plot(plotModels{i}.prX,plotModels{i}.prY);
+end
+hold off
+legend(modelNames,'Location','Best')
+xlabel('Recall')
+ylabel('Precision')
+title('Precision-Recall Curves')
+
+
+
+
+
+
+
+
+
+
+
+
+%% Explore feature space
+% Singular values and POD modes
+% S = zeros(size(data,2),1);
+% U = zeros(size(data));
+% for i=1:size(data,2)
+%     S(i)=norm(data(:,i));
+%     U(:,i)=data(:,i)/S(i);
+% end
+% plot(S)
+% plotFeatureSpace(U(class==1,:), U(class==0,:), [1 2 3]);
+% plotFeatureSpace(posData, negData, [3 10 29]);    
 %% Run model nIter times
 % nIter = 1;
 % disp(['Starting trials (',num2str(toc),')'])
@@ -100,24 +158,3 @@ disp(['Done classifying (',num2str(toc),')'])
 % % NOTE: if we are going to do cross validation MATLAB suggests:
 %     cvmodel = crossval(lda,'kfold',2);
 %     cverror = kfoldLoss(cvmodel)
-
-%% Plot
-plotModels = [lda ldaCosts ldaSyn];
-figure(1)
-hold on
-for c = arrayfun(@(s) [s.TPR,s.PPV], plotModels, 'un',0)
-    scatter(c{1}(1),c{1}(2))
-end
-% scatter(lda.TPR,lda.PPV)
-% scatter(ldaSyn.TPR,ldaSyn.PPV)
-% scatter(ldaCost.TPR,ldaCost.PPV)
-if runTree
-    scatter(tree.TPR,tree.PPV)
-end
-plot([0,1],[0,1],'-r')
-legend('show','location','northwest')
-hold off
-
-title('Precision-Recall')
-xlabel('Recall')
-ylabel('Precision')
