ee9f6a1d2d6eb345f73baf681837a1e92882dd08
tcunnington <taylor.cunnington@gmail.com>
2018-01-29 22:07:15 -0800

    avg recall and geometric mean

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from sklearn.metrics import recall_score, precision_score, f1_score
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import roc_curve, auc
+from imblearn.metrics import geometric_mean_score
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,7 +17,7 @@ import matplotlib.pyplot as plt
 #
 #################################################
 
-def split_by_class(df):
+def split_class(df):
     y = df['Class']
     x = df.drop(['Class'], axis=1)
     return x, y
@@ -50,6 +52,11 @@ def run_compare_sampling(Sampler,Model,train_x,train_y,test_x,test_y,**model_kwa
 #
 #################################################
 
+def truncate_dotdot(str):
+    if len(str) > 15:
+        str = str[0:13] + '...'
+    return str
+
 def get_class_name(instance):
     return str(instance).split("(")[0]
 
@@ -58,11 +65,18 @@ def print_class_counts(y_train):
     print(sorted(Counter(y_train).items()))
 
 def print_metrics(true, pred, *args):
+    print "Geometric mean:\t", geometric_mean_score(true, pred) # sqrt(recall * specificity), or sqrt(TPR * TNR)
     print "Recall: \t", recall_score(true, pred)
     print "Precision:\t", precision_score(true, pred)
-    print "F1 Score:\t", f1_score(true, pred)
+    # print "F1 Score:\t", f1_score(true, pred)
     print "Accuracy:\t", accuracy_score(true, pred)
     print "Confusion mat: \n", confusion_matrix(true, pred)
+    for a in args:
+        print a
+
+def print_avg_recall(model, X, y, *args):
+    scores = cross_val_score(model, X, y, scoring='recall', cv=5)
+    print 'Mean recall score ({0}): {1:0.2f}'.format(get_class_name(model), scores.mean())
     for a in args:
         print a
 
@@ -71,24 +85,6 @@ def print_metrics(true, pred, *args):
 #     Plot helpers
 #
 #################################################
-
-
-def plot_pr(model, test_x, test_y):
-    y_score = model.decision_function(test_x)
-
-    average_precision = average_precision_score(test_y, y_score)
-    precision, recall, _ = precision_recall_curve(test_y, y_score)
-
-    # TODO make more pretty?
-    plt.step(recall, precision, color='b', alpha=0.2, where='post')
-    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
-
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.ylim([0.0, 1.05])
-    plt.xlim([0.0, 1.0])
-    plt.title('Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
-    plt.show()
 
 
 def plot_features(f1, f2, df, ax=None):
@@ -123,19 +119,49 @@ def tile_random_features(feature_list, df):
 
     plt.show()
 
-def plot_roc(test_y, y_score):
+def plot_pr(model, test_x, test_y, ax=None, **plot_args):
+    if not ax:
+        plt.figure()
+        ax = plt.subplot(111)
+
+    y_score = model.decision_function(test_x)
+
+    average_precision = average_precision_score(test_y, y_score)
+    precision, recall, _ = precision_recall_curve(test_y, y_score)
+    pr_auc = auc(recall, precision)
+    model_name = truncate_dotdot(get_class_name(model))
+
+    # TODO make more pretty?
+    plt.step(recall, precision, label='{0}, AUPRC: {1:0.2f}'.format(model_name, pr_auc), **plot_args) #, color='b', alpha=0.2, where='post')
+    # plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+
+    ax.legend(loc = 'lower left')
+    ax.set_title('Precision-Recall curve')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlim([0.0, 1.0])
+
+    return ax
+
+def plot_roc(model, test_x, test_y, ax=None):
+    if not ax:
+        plt.figure()
+        ax = plt.subplot(111)
+
+    y_score = model.decision_function(test_x)
     fpr, tpr, _ = roc_curve(test_y, y_score)
     roc_auc = auc(fpr, tpr)
+    model_name = truncate_dotdot(get_class_name(model))
 
-    plt.figure()
     lw = 2
-    plt.plot(fpr, tpr, color='darkorange',
-             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
-    plt.show()
+    ax.plot(fpr, tpr, lw=lw, label='{0}, AUROC: {1:0.2f}'.format(model_name, roc_auc))
+    ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Receiver operating characteristic example')
+
+    ax.legend(loc="lower right")
+
